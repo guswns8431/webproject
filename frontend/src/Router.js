fce5d637c28a82a936779a40e24d43be435631cd
liketiger <edx2fire@likelion.org>
2019-12-30 18:30:08 +0900

    lat

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { HashRouter, Route } from "react-router-dom";
 import MyPage from "./MyPage";
 import Navigation from "./Navigation"
-import App from "./App";
+import Eval from "./Eval";
 
 function Router() {
     return (
     <HashRouter>
         <Navigation />
-        <Route path="/" exact={true} component={App} />        
+        <Route path="/" exact={true} component={Eval} />        
         <Route path="/MyPage" component={MyPage} />
     </HashRouter>
     );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Eval from "./Eval";
 import MyPage from "./MyPage";
 import { Login } from "./Login";
 import home from "./home";
