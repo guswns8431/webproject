00350b171907347cff285f1655489a354cfdcc07
liketiger <edx2fire@likelion.org>
2019-12-30 18:11:38 +0900

    d

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import home from "./home";
 import Router from "./Router";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Router />, document.getElementById("root"));
+ReactDOM.render(<Login />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
